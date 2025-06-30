b6623652c7cddf5f27c35e08a48dd07afe05215e
bipin9178 <bipinpkanzariya@gmail.com>
2025-06-30 05:03:53 +0000

    datatypes-summary file

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// # Primitive
+
+//  7 types : String ,Number , Boolearn , null, undefined , Symbol, BigInt
+
+const score = 100  
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3536546745243n
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions 
+
+const heros=["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "bipin",
+    age: 22,
+}
+
+const myFunction = function ()
+{
+    console.log("hello World");
+    
+}
+
+console.log(typeof id);
