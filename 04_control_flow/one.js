690452d205f95f443fecce7e78673a857c2ef931
bipin9178 <bipinpkanzariya@gmail.com>
2025-07-16 07:24:27 +0000

    condition file

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+// if
+const isUserLoggedIn=true
+const temperture= 41
+if (temperture=== 41) {
+    //console.log("less then 50");
+}
+    else{
+        //console.log("tamperature is greater then 50");
+        
+    }
+   
+//console.log("tamperature is greater then 50");
+
+//<,>,<=,>=,==,!=,===,!==
+
+// const score =200
+// if (score>100) {
+//     let power="fly"
+//     console.log(`user power:${power}`);
+    
+// }
+// console.log(`user power:${power}`);
+
+
+const balance =1000
+// if(balance>500) console.log("test"), console.log("test2");
+    
+// if(balance<500)
+// {
+//     console.log("less then 500");
+// }else if(balance<900){
+//      console.log("less then 900");
+// }
+// else if(balance<750){
+//      console.log("less then 750");
+// }
+// else{
+//     console.log("less then 1200"); 
+// }
+
+const userLoggedIn= true
+const debitCard=true
+const loggedInFromGoogle =false
+const loggedInFromEmail =true
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allows to buy course");
+}
+if (loggedInFromGoogle || loggedInFromEmail ){ 
+    console.log("User Logged in")
+}
