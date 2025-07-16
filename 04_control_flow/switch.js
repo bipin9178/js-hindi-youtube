5efca27569eead951d49b8f0a260a7d7ac3457d3
bipin9178 <bipinpkanzariya@gmail.com>
2025-07-16 09:07:15 +0000

    switch file

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,23 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 1:
+        console.log("frbruary");
+        break;
+    
+
+    default:
+        break;
+}
