61531608bb06f7b5431e8458d78df47086f89179
bipin9178 <bipinpkanzariya@gmail.com>
2025-07-16 09:28:38 +0000

    three.js

--- 04_control_flow/three.js
+++ 04_control_flow/three.js
@@ -4,3 +4,20 @@ for (const num of arr)
         console.log(num);
            
 }
+const greetings="hello World!"
+for (const greet of  greetings) 
+    {
+        console.log(` each in of ${greet}`);
+           
+}
+const map =new Map()
+map.set("IN","India");
+map.set("FR","frances");
+map.set("IN","India");
+console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key,':-',value);
+    
+    
+}
